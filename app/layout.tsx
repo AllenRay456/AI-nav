a0e87277c3b1d4ec22f36cbecdc434b89668e417
allenRay <yutianriyue@gmail.com>
2024-01-15 14:16:02 +0800

    chore: add SEO keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export const metadata: Metadata = {
     template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,
+  keywords: siteConfig.keywords,
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,10 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "AI指南针：好用免费的AI工具导航,包括ChatGPT4和Midjourney",
+  name: "AI指南针| 好用免费的AI工具导航,收录了国内外优质AI 工具,包括ChatGPT、Midjourney、AI对话聊天、AI绘画设计、AI视频制作、AI写作办公、AI编程开发、AI资讯等,轻松找到好用的AI工具和资源",
   description:
     "发现AI指南针 - 您的首选免费AI工具集合站点。涵盖ChatGPT等多种人工智能工具，我们的导航站助您轻松找到最适合的AI解决方案。提高效率，激发创造力，立即探索！",
+  keywords: 'AI导航,AI工具导航,人工智能,AI神器大全,AI工具大全,AI工具,AI神器,AI工具导航,ChatGPT免费资源,AIGC工具和应用,Midjourney创意工具,StableDiffusion技术,AI绘画, AI聊天机器人,综合AI工具集,人工智能工具箱,AI工具库,AI教程,开源AI工具,AI开源项目,AI模型,人工智能工具,开发者工具',
   mainNav: [
     {
       title: "Home",
