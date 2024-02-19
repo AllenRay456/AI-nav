34acdaa2f9d2803daedfd8ba0c20748935e34e91
allenRay <yutianriyue@gmail.com>
2024-02-19 19:38:19 +0800

    chore: SEO keywords

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,9 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "AI百宝箱-好用免费的AI工具导航,收录了国内外优质AI工具,包括ChatGPT、Midjourney",
-  description: "通用/垂直人工智能应用导航站,这里包含ChatGPT、Midjourney、AI聊天、AI绘画、AI音视频、AI写作办公、AI编程开发、AI资讯等先进AI工具,总有一款适合你",
-  keywords: 'AI导航,AI工具导航,ChatGPT账号,免费ChatGPT,垂直AI应用,通用AI应用,人工智能,AI神器大全,AI工具大全,AI工具,AI神器,AI工具导航,ChatGPT,ChatGPT4,GPTS,OpenAI,AIGC工具和应用,Midjourney,StableDiffusions,AI绘画,AI视频,AI语音,AI图像,AI聊天机器人,综合AI工具集,人工智能工具箱,AI工具库,AI教程,开源AI工具,AI开源项目,AI模型,人工智能工具,开发者工具,devTools',
+  name: "AI百宝箱-好用免费的AI工具导航,收录了国内外优质AI工具,包括ChatGPT、Midjourney、Sora、Perplexity、Bing Copilot、Coze、Devv_ 等等",
+  description: "AI百宝箱是一个通用/垂直人工智能应用导航站,这里包含ChatGPT、Midjourney、Sora、AI机器人、AI搜索、AI图像、AI视频、AI语音、AI插件、AI开源、AI生活、AI devtool、AI建站、AI学习、GPTs等先进AI工具,总有一款适合你",
+  keywords: 'AI导航,AI工具导航,ChatGPT,ChatGPT4,Midjourney,Sora,GPTS,OpenAI,Midjourney,StableDiffusions,免费ChatGPT,free chatgpt,垂直AI应用,通用AI应用,人工智能,AI神器,AI工具大全,AIGC工具和应用,AI机器人,AI搜索,AI图像,AI视频,AI语音,AI插件,AI开源,AI生活,AI devtool,AI建站,AI学习,AI教程,开源AI工具,AI模型,开发者工具',
   mainNav: [
     {
       title: "Home",
