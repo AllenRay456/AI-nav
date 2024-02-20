ec16f4efa81d2114ada7ae745dd1e7c13dd2d345
allenRay <yutianriyue@gmail.com>
2024-02-20 08:10:23 +0800

    chroe: SEO

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,9 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "AI百宝箱-好用免费的AI工具导航,收录了国内外优质AI工具,包括ChatGPT、Midjourney、Sora、Perplexity、Bing Copilot、Coze、Devv_ 等等",
-  description: "AI百宝箱是一个通用/垂直人工智能应用导航站,这里包含ChatGPT、Midjourney、Sora、AI机器人、AI搜索、AI图像、AI视频、AI语音、AI插件、AI开源、AI生活、AI devtool、AI建站、AI学习、GPTs等先进AI工具,总有一款适合你",
-  keywords: 'AI导航,AI工具导航,ChatGPT,ChatGPT4,Midjourney,Sora,GPTS,OpenAI,Midjourney,StableDiffusions,免费ChatGPT,free chatgpt,垂直AI应用,通用AI应用,人工智能,AI神器,AI工具大全,AIGC工具和应用,AI机器人,AI搜索,AI图像,AI视频,AI语音,AI插件,AI开源,AI生活,AI devtool,AI建站,AI学习,AI教程,开源AI工具,AI模型,开发者工具',
+  name: "AI百宝箱|AI工具导航|收录了优质AI工具|包括免费ChatGPT4、Midjourney、Sora",
+  description: "AI百宝箱是一个通用/垂直人工智能应用导航站,这里包含免费ChatGPT4、Midjourney、Sora、AI机器人、AI搜索、AI图像、AI视频、AI语音、AI插件、AI开源、AI生活、AI devtool、AI建站、AI学习、GPTs等先进AI工具,总有一款适合你",
+  keywords: 'ChatGPT,ChatGPT4,免费ChatGPT4,free chatgpt4,AI导航,AI工具导航,Midjourney,Sora,GPTS,OpenAI,StableDiffusions,垂直AI应用,通用AI应用,人工智能,AI神器,AI工具大全,AIGC工具和应用,AI机器人,AI搜索,AI图像,AI视频,AI语音,AI插件,AI开源,AI生活,AI devtool,AI建站,AI学习,AI教程,AI模型,开发者工具',
   mainNav: [
     {
       title: "Home",
