e30a9218a0d9cafd71c836f624af154d73dad64a
allenRay <yutianriyue@gmail.com>
2024-03-29 10:45:27 +0800

    chore: seo

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,9 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "OpenAI导航站|发现最好用的通用和垂直AI工具|包括免费的ChatGPT4、Midjourney、Sora",
-  description: "OpenAI导航站是一个通用和垂直AI工具导航站,这里包含免费的ChatGPT4、Midjourney、Sora、AI机器人、AI搜索、AI图像、AI视频、AI语音、AI插件、AI开源、AI生活、AI devtool、AI建站、AI学习、GPTs等先进AI工具,总有一款适合你",
-  keywords: ' OpenAI,ChatGPT,ChatGPT4,免费,free chatgpt4,AI导航,AI工具导航,Midjourney,Sora,GPTS,OpenAI,StableDiffusions,垂直AI应用,通用AI应用,人工智能,AI神器,AI工具大全,AIGC工具和应用,AI机器人,AI搜索,AI图像,AI视频,AI语音,AI插件,AI开源,AI生活,AI devtool,AI建站,AI学习,AI教程,AI模型,开发者工具',
+  name: "OpenAI导航站|发现最好用免费的通用和垂直AI工具|包括ChatGPT4、Midjourney、Sora、Suno、Claude3、文言一心等等",
+  description: "OpenAI导航站是一个通用和垂直AI工具导航站,这里包含免费的ChatGPT4、Midjourney、Sora、Suno、AI机器人、AI搜索、AI图像、AI视频、AI语音、AI插件、AI开源、AI生活、AI devtool、AI建站、AI学习、GPTs等先进AI工具,总有一款适合你",
+  keywords: ' OpenAI,ChatGPT,free chatgpt4,Claude3,Suno,Midjourney,Sora,GPTS,StableDiffusions,AI导航,AI工具导航,垂直AI应用,通用AI应用,人工智能,AI神器,AIGC工具,AI聊天机器人,AI搜索,AI图像,AI视频,AI语音,AI插件,AI开源,AI生活,AI devtool,AI建站,AI教育,AI教程,文言一心',
   mainNav: [
     {
       title: "Home",
