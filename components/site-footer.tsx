61289bb44cbdff3b213c57805d7041bfeb7b0d6b
allenRay <yutianriyue@gmail.com>
2024-05-04 09:19:05 +0800

    文案

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -4,9 +4,7 @@ export function SiteFooter() {
       <footer className="block py-4">
         <div className="mx-auto">
           <div className="mb-2 text-center md:mb-0">
-            人人都能享受到AI基础设施,一个也不能少
-            <br/>
-            尼古拉斯·赵四
+            发现最好用的垂直行业AI工具，助力社会中千千万万个你和我
           </div>
         </div>
       </footer>
