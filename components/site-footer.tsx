860a898804f5f3ba497bdbbfdc664265e5d02e39
allenRay <yutianriyue@gmail.com>
2024-06-21 07:44:43 +0800

    China AI Top50

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -9,10 +9,11 @@ export function SiteFooter() {
             发现最新最好用的通用和垂直 AI 工具
           </div>
           <Link href='/about' className="block text-center text-gray-800">关于我们</Link>
-          <div className="flex justify-center gap-x-4 text-red-400">
+          <div className="flex justify-center gap-x-1 text-red-400">
             <Link href="https://waytoagi.feishu.cn/wiki/QPe5w5g7UisbEkkow8XcDmOpn8e" target="blank">通往 AGI 之路</Link>|
             <Link href="https://baoyu.io/blog" target="blank">宝玉的分享</Link>｜
-            <Link href="/rank/aitop100" target="blank">全球AI工具Top100</Link>
+            <Link href="/rank/aitop100" target="blank">全球AI工具Top100</Link>｜
+            <Link href="/rank/china-ai-top50" target="blank">中国AI工具Top50</Link>
           </div>
         </div>
       </footer>
